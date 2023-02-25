3dcc0b17e599f239a31518a431b7befd6525ed2a
brandonhjh <brandonhong61@gmail.com>
2023-02-25 14:44:56 +0800

    Created and added react base

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Visualisation1 from './components/Visualisation1';
+
+const App = () => {
+  return (
+    <>
+      <div className="Title">
+        <h1>the boys</h1>
+      </div>
+      <Visualisation1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Visualisation1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './visualisation1.css'
+
+const Visualisation1 = () => {
+  return (
+    <div>Visualisation1</div>
+  )
+}
+
+export default Visualisation1
